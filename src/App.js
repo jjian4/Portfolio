518c6ebd2f9cd92c19f9777657c84433cfe7a477
jjian4 <jjian@umich.edu>
2019-11-27 15:17:09 -0500

    Drafted navbar

--- src/App.js
+++ src/App.js
@@ -1,35 +1,44 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import './App.css';
 import Home from './components/Home';
 import About from './components/About';
+import Experience from './components/Experience';
+import Projects from './components/Projects';
 
 function App() {
     return (
         <Router>
-            <div>
-                <div className='navbar'>
-                    <ul>
-                        <li>
-                            <Link to='/'>Home</Link>
-                        </li>
-                        <li>
-                            <Link to='/about'>About</Link>
-                        </li>
-                    </ul>
-                </div>
+            <ul className='navbar'>
+                <li className='navItem'>
+                    <Link to='/'>HOME</Link>
+                </li>
+                <li className='navItem'>
+                    <Link to='/about'>ABOUT</Link>
+                </li>
+                <li className='navItem'>
+                    <Link to='/experience'>EXPERIENCE</Link>
+                </li>
+                <li className='navItem'>
+                    <Link to='/projects'>PROJECTS</Link>
+                </li>
+            </ul>
 
-                <hr />
-
-                {/* Renders Route where path matches current url */}
-                <Switch>
-                    <Route exact path='/'>
-                        <Home />
-                    </Route>
-                    <Route path='/about'>
-                        <About />
-                    </Route>
-                </Switch>
-            </div>
+            {/* Renders Route where path matches current url */}
+            <Switch>
+                <Route exact path='/'>
+                    <Home />
+                </Route>
+                <Route path='/about'>
+                    <About />
+                </Route>
+                <Route path='/experience'>
+                    <Experience />
+                </Route>
+                <Route path='/projects'>
+                    <Projects />
+                </Route>
+            </Switch>
         </Router>
     );
 }
