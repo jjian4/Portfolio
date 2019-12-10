380ac6e09f04577654b8295558772b57c10cbc94
jjian4 <jjian@umich.edu>
2019-12-10 18:11:43 -0500

    Made Home class component

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -8,74 +8,78 @@ import Fade from 'react-reveal/Fade';
 
 const scrollToRef = ref => window.scrollTo(0, ref.current.offsetTop);
 
-const Home = () => {
-    const homeDividerRef = React.createRef();
-    const executeScroll = () => scrollToRef(homeDividerRef);
+class Home extends React.Component {
+    homeDividerRef = React.createRef();
+    executeScroll = () => {
+        scrollToRef(this.homeDividerRef);
+    };
 
-    return (
-        <div className='home'>
-            <div className='landingScreen'>
-                <div className='backgroundVideoWrap'>
-                    <video playsInline autoPlay muted loop>
-                        <source
-                            src={require('../../static/backgrounds/clouds.webm')}
-                            type='video/webm'
-                        />
-                        <source
-                            src={require('../../static/backgrounds/clouds.mp4')}
-                            type='video/mp4'
-                        />
-                    </video>
-                </div>
-                <div className='videoOverlay'></div>
-                <div className='landingContent'>
+    render() {
+        return (
+            <div className='home'>
+                <div className='landingScreen'>
+                    <div className='backgroundVideoWrap'>
+                        <video playsInline autoPlay muted loop id='vid'>
+                            <source
+                                src={require('../../static/backgrounds/clouds.webm')}
+                                type='video/webm'
+                            />
+                            <source
+                                src={require('../../static/backgrounds/clouds.mp4')}
+                                type='video/mp4'
+                            />
+                        </video>
+                    </div>
+                    <div className='videoOverlay'></div>
+                    <div className='landingContent'>
+                        <div className='container'>
+                            <div className='landingTitle'>James Jiang</div>
+                            <Fade>
+                                <div className='landingSubtitle'>
+                                    Full Stack Engineer
+                                </div>
+                                <div className='landingButtons row'>
+                                    <span className='col-md-4'>
+                                        <button
+                                            className='landingButton'
+                                            onClick={this.executeScroll}
+                                        >
+                                            ABOUT
+                                        </button>
+                                    </span>
+                                    <span className='col-md-4'>
+                                        <Link to='/experience'>
+                                            <button className='landingButton'>
+                                                EXPERIENCE
+                                            </button>
+                                        </Link>
+                                    </span>
+                                    <span className='col-md-4'>
+                                        <Link to='/projects'>
+                                            <button className='landingButton'>
+                                                PROJECTS
+                                            </button>
+                                        </Link>
+                                    </span>
+                                </div>
+                            </Fade>
+                        </div>
+                    </div>
                     <div className='container'>
-                        <div className='landingTitle'>James Jiang</div>
-                        <Fade>
-                            <div className='landingSubtitle'>
-                                Full Stack Engineer
-                            </div>
-                            <div className='landingButtons row'>
-                                <span className='col-md-4'>
-                                    <button
-                                        className='landingButton'
-                                        onClick={executeScroll}
-                                    >
-                                        ABOUT
-                                    </button>
-                                </span>
-                                <span className='col-md-4'>
-                                    <Link to='/experience'>
-                                        <button className='landingButton'>
-                                            EXPERIENCE
-                                        </button>
-                                    </Link>
-                                </span>
-                                <span className='col-md-4'>
-                                    <Link to='/projects'>
-                                        <button className='landingButton'>
-                                            PROJECTS
-                                        </button>
-                                    </Link>
-                                </span>
-                            </div>
-                        </Fade>
+                        <div className='landingArrow'>
+                            <button onClick={this.executeScroll}>
+                                <FontAwesomeIcon icon={faAngleDoubleDown} />
+                            </button>
+                        </div>
                     </div>
                 </div>
-                <div className='container'>
-                    <div className='landingArrow'>
-                        <button onClick={executeScroll}>
-                            <FontAwesomeIcon icon={faAngleDoubleDown} />
-                        </button>
-                    </div>
-                </div>
+
+                <div ref={this.homeDividerRef} id='homeDivider' />
+
+                <About />
             </div>
-
-            <div ref={homeDividerRef} id='homeDivider' />
-
-            <About />
-        </div>
-    );
-};
+        );
+    }
+}
 
 export default Home;
